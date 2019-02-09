5ded379856c1ab071e2ac6f8a4f9e2bcb7ebcd9f
ercembu <ercembulut@gmail.com>
2019-02-09 03:05:06 +0300

    waiting on cuda

--- lchess/train.py
+++ lchess/train.py
@@ -65,3 +65,39 @@ class Net(nn.Module):
 
     # value output
         return F.tanh(x)
+
+if __name__ == "__main__":
+    device = "cuda"
+
+    
+
+    chess_dataset = ChessValueDataset()
+    train_loader = torch.utils.data.DataLoader(chess_dataset, batch_size=256, shuffle = True)
+    model = Net()
+    model.cuda()
+    optimizer = optim.Adam(model.parameters())
+    floss = nn.MSELoss()
+
+    model.train()
+
+    for epoch in range(100):
+        all_loss = 0
+        num_loss = 0
+        for batch_idx, (data,target) in enumerate(train_loader):
+            target = target.unsqueeze(-1)
+            data, target = data.to(device), target.to(device)
+            data = data.float()
+            target = target.float()
+
+            optimizer.zero_grad()
+            output = model(data)
+
+            loss = floss(output, target)
+            loss.backward()
+            optimizer.step()
+
+            all_loss += loss.item()
+            num_loss += 1
+
+        print("%5d: %f" % (epoch, all_loss/num_loss))
+        torch.save(model.state_dict(), "nets/value.pth")
